7aeb71f9cf97c3cc40d77a76d29b67dfb10dc0d1
Jan Dudek <jd@jandudek.com>
2014-07-23 18:39:21 +0200

    Improved notifications

--- lib/githug/notification.rb
+++ lib/githug/notification.rb
@@ -5,11 +5,30 @@ module Githug
   class Notification
     def initialize(options)
       @success = options[:success]
+      @level   = options[:level]
     end
 
     def deliver
-      uri = URI.parse("http://requestb.in/ycqm45yc")
-      response = Net::HTTP.post_form(uri, { "email" => "me@example.com", "success" => @success })
+      uri = URI.parse(endpoint)
+      data = {
+        "user_email" => user_email,
+        "user_name" => user_name,
+        "success" => @success,
+        "level_name" => @level.level_name
+      }
+      response = Net::HTTP.post_form(uri, data)
+    end
+
+    def user_name
+      `git config --global user.name`.strip
+    end
+
+    def user_email
+      `git config --global user.email`.strip
+    end
+
+    def endpoint
+      ENV["GITHUG_NOTIFY_URL"] or raise "Please set GITHUG_NOTIFY_URL env variable."
     end
   end
 end
